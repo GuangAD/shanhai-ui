964d45a1974623ca24ef5f8ab455fb8ac6b6f1b3
ggc <1536576466@qq.com>
2022-02-10 22:59:30 +0800

    feat: 为shanhai-ui构建测试程序

--- script/verify-commit.js
+++ script/verify-commit.js
@@ -4,7 +4,6 @@ const commitRE =
   /^(feat|fix|docs|style|refactor|perf|test|workflow|build|ci|chore|release|workflow)(\(.+\))?: .{1,50}/
 
 if (!commitRE.test(msg)) {
-  console.log()
   console.error(`
         不合法的 commit 消息格式。
         请查看 git commit 提交规范：https://github.com/woai3c/Front-end-articles/blob/master/git%20commit%20style.md
